793e4064dd1a9cb256fa7ed863ba37b586ab5c30
edriso <edriso.dev@gmail.com>
2024-01-08 21:37:04 +0200

    add register link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,13 +17,14 @@
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen">
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex flex-col justify-center items-center">
-                    <h1 class="font-light text-8xl">MyGym</h1>
+                    <h1 class="font-light text-8xl">My Gym</h1>
                     <div class="mt-8">
                     @if (Route::has('login'))
                         @auth
                             <a href="{{ route('dashboard') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150">Dashboard</a>
                         @else
-                            <a href="{{ route('login') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150">Log In</a>
+                        <a href="{{ route('login') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150 mr-4">Log In</a>
+                        <a href="{{ route('register') }}" class="px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150">Register</a>
                         @endauth
                     @endif
                     </div>
